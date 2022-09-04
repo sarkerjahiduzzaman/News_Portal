fe90e31870c289b209e0be20a6ffb62334d6dda0
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-09-04 07:15:53 +0600

    menubar call function

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ const menuBarFunction=()=>{
 
 }
 // Api link handalink function end
+
+menuBarFunction()
+//News find by id start here
+const findid=(id)=>{
+  loadigFunction(true)
+  fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
+  .then(res=>res.json())
+  .then(data=>display2(data.data))
+  .catch(error=>console.log(error))
+}
+
